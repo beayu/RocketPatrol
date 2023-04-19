c82384df81fd00fceb102f2303332de223544ccb
Beatrice Yu <besyu@ucsc.edu>
2023-04-19 15:40:34 -0700

    game over

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -9,6 +9,8 @@ class Rocket extends Phaser.GameObjects.Sprite {
       scene.add.existing(this);
       this.isFiring = false; 
       this.moveSpeed = 2; 
+
+      this.sfxRocket = scene.sound.add('sfx_rocket'); // add rocket sfx
     }
 
     update() {
@@ -21,15 +23,16 @@ class Rocket extends Phaser.GameObjects.Sprite {
             }
         }
         // fire button 
-        if(Phaser.Input.Keyboard.JustDown(keyF)) {
+        if (Phaser.Input.Keyboard.JustDown(keyF)) {
             this.isFiring = true; 
+            this.sfxRocket.play();  // play sfx
         }
         // if fired, move up 
-        if(this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
+        if (this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
             this.y -= this.moveSpeed; 
         }
         // reset on miss 
-        if(this.y <= borderUISize * 3 + borderPadding) {
+        if (this.y <= borderUISize * 3 + borderPadding) {
             this.isFiring = false; 
             this.y = game.config.height - borderUISize - borderPadding; 
         }
