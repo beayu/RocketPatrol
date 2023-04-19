a176ece60c37497ba868795294034cb88e3c4566
Beatrice Yu <besyu@ucsc.edu>
2023-04-19 14:57:51 -0700

    explosion

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -57,15 +57,15 @@ class Play extends Phaser.Scene {
         // check collisions 
         if (this.checkCollision(this.p1Rocket, this.ship03)) {
             this.p1Rocket.reset(); 
-            this.ship03.reset(); 
+            this.shipExplode(this.ship03); 
         }
         if (this.checkCollision(this.p1Rocket, this.ship02)) {
             this.p1Rocket.reset();
-            this.ship02.reset();
+            this.shipExplode(this.ship02);
         }
         if (this.checkCollision(this.p1Rocket, this.ship01)) {
             this.p1Rocket.reset();
-            this.ship01.reset();
+            this.shipExplode(this.ship01);
         }
     }
 
