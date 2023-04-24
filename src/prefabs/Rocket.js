aedfc746a8d52451ce72cdaa13e8836ec7fe9eac
Beatrice Yu <besyu@ucsc.edu>
2023-04-24 16:03:32 -0700

    fix sound

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -23,7 +23,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
             }
         }
         // fire button 
-        if (Phaser.Input.Keyboard.JustDown(keyF)) {
+        if (Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
             this.isFiring = true; 
             this.sfxRocket.play();  // play sfx
         }
